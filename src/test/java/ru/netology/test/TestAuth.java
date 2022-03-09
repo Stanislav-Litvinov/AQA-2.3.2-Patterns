2d8cb11cf9f3af114abc8aca74af9a5c5187682f
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2022-03-09 18:47:54 +0300

    Update TestAuth.java

--- src/test/java/ru/netology/test/TestAuth.java
+++ src/test/java/ru/netology/test/TestAuth.java
@@ -15,10 +15,6 @@ import static ru.netology.data.DataGenerator.getRandomPassword;
 public class TestAuth {
     @BeforeEach
     void setup() {
-        WebDriverManager.chromedriver().driverVersion("99").setup();
-        Configuration.browser = "chrome";
-        Configuration.headless = true;
-        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
 
